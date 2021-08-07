4ee72b76d4e0965a2c5ee428a01a797d8598f6a3
Snir Yacoby <yacobysn@gmail.com>
2021-08-07 19:27:44 +0300

    End of lecture 196.

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/widgets/product_item.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
